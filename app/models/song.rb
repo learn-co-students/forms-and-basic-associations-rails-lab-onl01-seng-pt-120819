862d0a6c58879a09570530c935bb9732a4fddb71
Amanda <main@Amandas-MacBook-Air.local>
2020-05-02 12:11:14 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,13 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
   
+  accepts_nested_attributes_for :notes, :reject_if => proc { |attrs| attrs[:content].blank? }
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    artist.try(:name)
+  end
+
 end
