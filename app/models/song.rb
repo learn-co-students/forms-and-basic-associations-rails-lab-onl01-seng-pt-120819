98d085ceeb368d2984ce46cd5131b4d978b53046
github username <github email address>
2020-05-14 18:52:00 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+  accepts_nested_attributes_for :notes
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
@@ -11,4 +12,5 @@ class Song < ActiveRecord::Base
   def artist_name
     self.artist ? self.artist.name : nil
   end
+
 end
